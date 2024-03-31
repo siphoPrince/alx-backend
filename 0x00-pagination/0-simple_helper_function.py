0a9fd5732194505439db3708837e130d52afc8fc
siphoprince <mabirimisesipho@gmail.com>
2024-03-31 01:50:15 -0700

    helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""calculations"""
+
+
+def index_range(page, page_size):
+    """
+    Computes the start and end index for a given page and page size.
+
+    Args:
+        page (int): The page number (1-indexed).
+        page_size (int): The number of items per page.
+
+    Returns:
+        tuple: A tuple containing the start index and end index for the given page.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
