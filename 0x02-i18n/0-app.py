d47021cc709c7c0d8d4cdf47b95d521a24bad42b
siphoprince <mabirimisesipho@gmail.com>
2024-04-09 08:53:09 -0700

    index route

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,9 +8,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-  return render_template('index.html')
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """ sends a GET /
+    Return:
+        Hello world
+    """
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run(host="0.0.0.0", port="5000")
