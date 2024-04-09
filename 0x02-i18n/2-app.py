93ea7ebb56290a04513dd76a26e86d8be7387733
siphoprince <mabirimisesipho@gmail.com>
2024-04-09 09:36:20 -0700

    index file error fix

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,30 +1,23 @@
 #!/usr/bin/env python3
 """making main file"""
 
-
 from flask import Flask, render_template, request
-from flask.ext.babel import Babel, gettext
+from flask_babel import Babel
 
-# Create a configuration class
-class Config:
-  LANGUAGES = ["en", "fr"]  # Supported languages
-  BABEL_DEFAULT_LOCALE = "en"  # Default locale (English)
-  BABEL_DEFAULT_TIMEZONE = "UTC"  # Default timezone
 
-# Initialize Flask app and Babel object
 app = Flask(__name__)
-app.config.from_object(Config)
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
-  return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
 def index():
-  welcome_message = gettext('Welcome to Holberton')  # Mark for translation
-  return render_template('2-index.html', welcome_message=welcome_message)
+    return render_template('2-index.html')
+
 
 if __name__ == '__main__':
-  app.run(debug=True)
-
+    app.run(debug=True)
