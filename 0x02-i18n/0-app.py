6ec3acf1d9301e2055e3b621db75db5fdbef66de
siphoprince <mabirimisesipho@gmail.com>
2024-04-09 09:31:04 -0700

    fixing route

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,14 +8,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def index() -> str:
-    """ sends a GET /
-    Return:
-        Hello world
-    """
-    return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-  app.run(host="0.0.0.0", port="5000")
+    app.run(debug=True)
