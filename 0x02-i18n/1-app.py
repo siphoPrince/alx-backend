69dcc4c3a3068ffaa663042ffbec4a46436edd69
siphoprince <mabirimisesipho@gmail.com>
2024-04-09 09:25:48 -0700

    files

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,27 +2,19 @@
 """ Store it in a module-level variable named babel"""
 
 
-from flask import Flask, render_template
-from flask.ext.babel import Babel,gettext
+from flask import Flask
+from flask_babel import Babel
+
+
+app = Flask(__name__)
 
 
 class Config:
-    """main class"""
-  LANGUAGES = ["en", "fr"]
-  BABEL_DEFAULT_LOCALE = "en"
-  BABEL_DEFAULT_TIMEZONE = "UTC"
+    """config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
-app = Flask(__name__)
-app.config.from_object(Config)
+
 babel = Babel(app)
-
-
-@app.route('/')
-def index():
-  welcome_message = gettext('Welcome to Holberton')
-  return render_template('index.html', welcome_message=welcome_message)
-
-
-if __name__ == '__main__':
-  app.run(debug=True)
-
+app.config.from_object(Config)
