639a7f0c6c24610b2c711fd93cd78d8e71382f11
siphoprince <mabirimisesipho@gmail.com>
2024-05-08 05:02:01 -0700

    function return errer fix

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,25 @@
+const kue = require("kue");
+
+const queue = kue.createQueue();
+
+const JobData = {
+  phoneNumber: "4153518780",
+  message: "This is the code to verify your account",
+};
+
+const job = queue.create("push_notification_code", JobData).save((error) => {
+  if (!error) console.log(`Notification job created: ${job.id}`);
+});
+
+job.on("complete", () => {
+  console.log("Notification job completed");
+});
+
+job.on("failed", () => {
+  console.log("Notification job failed");
+});
+
+
+job.on("progress", () => {
+  console.log("Notification job failed");
+});
